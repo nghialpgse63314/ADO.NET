fd376558a575ad9f25d89f0c5656dfe1cd020c4f
nghialpgse63314 <48762744+nghialpgse63314@users.noreply.github.com>
2019-03-20 21:47:29 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace Demo4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtCarId = new System.Windows.Forms.TextBox();
+            this.txtColor = new System.Windows.Forms.TextBox();
+            this.txtMake = new System.Windows.Forms.TextBox();
+            this.txtPetName = new System.Windows.Forms.TextBox();
+            this.bAdd = new System.Windows.Forms.Button();
+            this.bUpdate = new System.Windows.Forms.Button();
+            this.bDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(56, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Inventory";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 46);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(322, 228);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.Click += new System.EventHandler(this.dataGridView1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(362, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "CarID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(362, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Make";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(362, 146);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Color";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(362, 192);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "PetName";
+            // 
+            // txtCarId
+            // 
+            this.txtCarId.Location = new System.Drawing.Point(434, 63);
+            this.txtCarId.Name = "txtCarId";
+            this.txtCarId.Size = new System.Drawing.Size(207, 20);
+            this.txtCarId.TabIndex = 6;
+            // 
+            // txtColor
+            // 
+            this.txtColor.Location = new System.Drawing.Point(434, 146);
+            this.txtColor.Name = "txtColor";
+            this.txtColor.Size = new System.Drawing.Size(207, 20);
+            this.txtColor.TabIndex = 7;
+            // 
+            // txtMake
+            // 
+            this.txtMake.Location = new System.Drawing.Point(434, 107);
+            this.txtMake.Name = "txtMake";
+            this.txtMake.Size = new System.Drawing.Size(207, 20);
+            this.txtMake.TabIndex = 8;
+            // 
+            // txtPetName
+            // 
+            this.txtPetName.Location = new System.Drawing.Point(434, 185);
+            this.txtPetName.Name = "txtPetName";
+            this.txtPetName.Size = new System.Drawing.Size(207, 20);
+            this.txtPetName.TabIndex = 9;
+            // 
+            // bAdd
+            // 
+            this.bAdd.Location = new System.Drawing.Point(365, 231);
+            this.bAdd.Name = "bAdd";
+            this.bAdd.Size = new System.Drawing.Size(75, 23);
+            this.bAdd.TabIndex = 10;
+            this.bAdd.Text = "Add new";
+            this.bAdd.UseVisualStyleBackColor = true;
+            this.bAdd.Click += new System.EventHandler(this.bAdd_Click);
+            // 
+            // bUpdate
+            // 
+            this.bUpdate.Location = new System.Drawing.Point(470, 231);
+            this.bUpdate.Name = "bUpdate";
+            this.bUpdate.Size = new System.Drawing.Size(75, 23);
+            this.bUpdate.TabIndex = 11;
+            this.bUpdate.Text = "Update";
+            this.bUpdate.UseVisualStyleBackColor = true;
+            this.bUpdate.Click += new System.EventHandler(this.bUpdate_Click);
+            // 
+            // bDelete
+            // 
+            this.bDelete.Location = new System.Drawing.Point(566, 231);
+            this.bDelete.Name = "bDelete";
+            this.bDelete.Size = new System.Drawing.Size(75, 23);
+            this.bDelete.TabIndex = 12;
+            this.bDelete.Text = "Delete";
+            this.bDelete.UseVisualStyleBackColor = true;
+            this.bDelete.Click += new System.EventHandler(this.bDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 324);
+            this.Controls.Add(this.bDelete);
+            this.Controls.Add(this.bUpdate);
+            this.Controls.Add(this.bAdd);
+            this.Controls.Add(this.txtPetName);
+            this.Controls.Add(this.txtMake);
+            this.Controls.Add(this.txtColor);
+            this.Controls.Add(this.txtCarId);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtCarId;
+        private System.Windows.Forms.TextBox txtColor;
+        private System.Windows.Forms.TextBox txtMake;
+        private System.Windows.Forms.TextBox txtPetName;
+        private System.Windows.Forms.Button bAdd;
+        private System.Windows.Forms.Button bUpdate;
+        private System.Windows.Forms.Button bDelete;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+          
+        }
+
+        public void loadData()
+        {
+            string connString = @"uid=sa;pwd=123456789;Initial Catalog=Cars;Data Source=NGHIALPGSE63314\LPGN";
+            SqlConnection conn = new SqlConnection(connString);
+            string query = "select * from Inventory ";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            try
+            {
+                if (conn.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+                da.Fill(dt);
+            }
+            catch (SqlException e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            dataGridView1.DataSource = dt;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loadData();
+        }
+
+        private void dataGridView1_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                txtCarId.Text = dataGridView1.SelectedRows[0].Cells["CarID"].Value.ToString();
+                txtMake.Text = dataGridView1.SelectedRows[0].Cells["Make"].Value.ToString();
+                txtColor.Text = dataGridView1.SelectedRows[0].Cells["Color"].Value.ToString();
+                txtPetName.Text = dataGridView1.SelectedRows[0].Cells["PetNames"].Value.ToString();
+            }
+        }
+
+        private void bAdd_Click(object sender, EventArgs e)
+        {
+            string connString = @"uid=sa;pwd=123456789;Initial Catalog=Cars;Data Source=NGHIALPGSE63314\LPGN";
+            SqlConnection conn = new SqlConnection(connString);
+            string query = "insert Inventory values(@CarID,@Make,@Color,@PetNames)";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@CarID", Convert.ToInt32(txtCarId.Text));
+            cmd.Parameters.AddWithValue("@Make", txtMake.Text);
+            cmd.Parameters.AddWithValue("@Color",txtColor.Text);
+            cmd.Parameters.AddWithValue("@PetNames", txtPetName.Text);
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            try
+            {
+                int result = cmd.ExecuteNonQuery();
+                if (result > 0)
+                {
+                    loadData();
+                }
+                else
+                {
+                    MessageBox.Show("Insert failed!!!");
+                }
+            }
+            catch (Exception se)
+            {
+                throw new Exception(se.Message);
+            }
+        }
+
+        private void bUpdate_Click(object sender, EventArgs e)
+        {
+            string connString = @"uid=sa;pwd=123456789;Initial Catalog=Cars;Data Source=NGHIALPGSE63314\LPGN";
+            SqlConnection conn = new SqlConnection(connString);
+            string query = "update Inventory set Make = @Make,Color = @Color,PetNames =@PetNames where CarID = @CarID";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@CarID", Convert.ToInt32(txtCarId.Text));
+            cmd.Parameters.AddWithValue("@Make", txtMake.Text);
+            cmd.Parameters.AddWithValue("@Color", txtColor.Text);
+            cmd.Parameters.AddWithValue("@PetNames", txtPetName.Text);
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            try
+            {
+                int result = cmd.ExecuteNonQuery();
+                if (result > 0)
+                {
+                    loadData();
+                }
+                else
+                {
+                    MessageBox.Show("Update failed");
+                }
+            }
+            catch (Exception se)
+            {
+                MessageBox.Show(se.Message);
+            }
+        }
+
+        private void bDelete_Click(object sender, EventArgs e)
+        {
+            string connString = @"uid=sa;pwd=123456789;Initial Catalog=Cars;Data Source=NGHIALPGSE63314\LPGN";
+            SqlConnection conn = new SqlConnection(connString);
+            string query = "delete Inventory where CarID = @CarID";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@CarID", Convert.ToInt32(txtCarId.Text));
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            try
+            {
+                int result = cmd.ExecuteNonQuery();
+                if (result > 0)
+                {
+                    loadData();
+                }
+                else
+                {
+                    MessageBox.Show("Delete failed");
+                }
+            }
+            catch (Exception se)
+            {
+                MessageBox.Show(se.Message);
+            }
+        }
+    }
+}
